65ca889db7b4f1018c0db9dcbf91a6ac2e66e118
danstuken <danstuken@gmail.com>
2015-08-04 15:42:49 +0100

    Added NUnit tests...not sucessfully so far :-\

---
+++ FredsPetShop/Tests.fs
@@ -0,0 +1,16 @@
+﻿namespace FredsPetShop.Tests
+
+open NUnit.Framework
+open FsUnit
+
+
+[<TestFixture>]
+module PetShopTests =
+
+    let addToTakings takingsSoFar transactionCost =
+        (takingsSoFar + transactionCost)
+
+    [<Test>]
+    let testAddToDailyTakings =
+        let result = addToTakings 3 2
+        Assert.AreEqual(5, result)
