10fa69ea7f78a5ea4871185bdff44e183651a9d3
danstuken <danstuken@gmail.com>
2015-08-12 13:45:16 +0100

    Added test for collection of VAT results. Hit upon the equality of floats issue...solution isn't great as lose the detail in the error message.

--- FredsPetShop/PetShopTests.fs
+++ FredsPetShop/PetShopTests.fs
@@ -39,26 +39,61 @@ let beastieLegTax (beastie: Beastie) =
     | FourLeggedBeastie b -> legTaxForFourLegs b.SalePrice
     | EightLeggedBeastie b -> legTaxForEightLegs b.SalePrice
 
+let sumOfBeastieLegTax (beasties: Beastie[]) =
+    beasties
+    |> Array.map (fun b -> beastieLegTax b)
+    |> Array.sum
+
+let sumOfBeastieVat (beasties: Beastie[]) =
+    beasties
+    |> Array.map (fun b -> beastieVat b)
+    |> Array.sum
+
+let isInRange expectedFloat actualFloat =
+    let maxAcceptableValue = expectedFloat + 0.0001
+    let minAcceptableValue = expectedFloat - 0.0001
+    actualFloat <= maxAcceptableValue && actualFloat >= minAcceptableValue
+
 [<Fact>]
 let vatAt18Percent_ShouldBe_18Percent_Of_SalePrice() =
-    Assert.Equal(4.5, vatAt18Percent 25.0)
+    Assert.True(isInRange 4.5 (vatAt18Percent 25.0))
 
 [<Fact>]
 let beastieVat_ShouldBe_18Percent_Of_BeastieSalePrice() =
     let b = TwoLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(4.5, (beastieVat b))
+    Assert.True(isInRange 4.5 (beastieVat b))
 
 [<Fact>]
 let beastieLegTax_ShouldBe_20Percent_Of_TwoLeggedBeastieSalePrice() =
     let b = TwoLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(5.0, (beastieLegTax b))
+    Assert.True(isInRange 5.0 (beastieLegTax b))
 
 [<Fact>]
 let beastieLegTax_ShouldBe_40Percent_Of_FourLeggedBeastieSalePrice() =
     let b = FourLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(10.0, (beastieLegTax b))
+    Assert.True(isInRange 10.0 (beastieLegTax b))
 
 [<Fact>]
 let beastieLegTax_ShouldBe_80Percent_Of_EightLeggedBeastieSalePrice() =
     let b = EightLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(20.0, (beastieLegTax b))
+    Assert.True(isInRange 20.0 (beastieLegTax b))
+
+[<Fact>]
+let beastieCollectionLegTax_ShouldBe_SumOfAllLegTaxInCollection() =
+    let collectionOfB = 
+        [|
+            EightLeggedBeastie { SalePrice = 20.0 }
+            TwoLeggedBeastie { SalePrice = 20.0 }
+            FourLeggedBeastie { SalePrice = 20.0 }
+        |]
+    Assert.True(isInRange 28.0 (sumOfBeastieLegTax collectionOfB))
+
+[<Fact>]
+let beastieCollectionVat_ShouldBe_SumOfAllVatInCollection() =
+    let collectionOfB = 
+        [|
+            EightLeggedBeastie { SalePrice = 20.0 }
+            TwoLeggedBeastie { SalePrice = 20.0 }
+            FourLeggedBeastie { SalePrice = 20.0 }
+        |]
+    Assert.True(isInRange 10.8 (sumOfBeastieVat collectionOfB))
