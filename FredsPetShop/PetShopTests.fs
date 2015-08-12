4390717da93b2536bb715b1b62a9a6d22aac9f68
danstuken <danstuken@gmail.com>
2015-08-12 07:26:07 +0100

    Simple refactor - need to encapsulate the things that change more (tax rate, e.g.)

--- FredsPetShop/PetShopTests.fs
+++ FredsPetShop/PetShopTests.fs
@@ -14,6 +14,12 @@ type Beastie =
 let vatAt18Percent salePrice =
     salePrice * 0.18
 
+let legTaxForTwoLegs salePrice =
+    salePrice * 0.2
+
+let legTaxForFourLegs salePrice =
+    salePrice * 0.4
+
 let beastieVat (beastie:Beastie) =
     match beastie with
     | TwoLeggedBeastie b -> vatAt18Percent b.SalePrice
@@ -21,8 +27,8 @@ let beastieVat (beastie:Beastie) =
 
 let beastieLegTax (beastie: Beastie) =
     match beastie with
-    | TwoLeggedBeastie b -> b.SalePrice * 0.2
-    | FourLeggedBeastie b -> b.SalePrice * 0.4
+    | TwoLeggedBeastie b -> legTaxForTwoLegs b.SalePrice
+    | FourLeggedBeastie b -> legTaxForFourLegs b.SalePrice
 
 [<Fact>]
 let vatAt18Percent_ShouldBe_18Percent_Of_SalePrice() =
@@ -36,10 +42,10 @@ let beastieVat_ShouldBe_18Percent_Of_BeastieSalePrice() =
 [<Fact>]
 let beastieLegTax_ShouldBe_20Percent_Of_TwoLeggedBeastieSalePrice() =
     let b = TwoLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(2.5, (beastieLegTax b))
+    Assert.Equal(5.0, (beastieLegTax b))
 
 [<Fact>]
 let beastieLegTax_ShouldBe_40Percent_Of_FourLeggedBeastieSalePrice() =
     let b = FourLeggedBeastie { SalePrice = 25.0 }
-    Assert.Equal(5.0, (beastieLegTax b))
+    Assert.Equal(10.0, (beastieLegTax b))
 
